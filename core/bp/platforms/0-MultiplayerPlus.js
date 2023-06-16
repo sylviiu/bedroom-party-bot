1f8bc2bf479affb43bf6f4e68b68234cee070b46
Syl <hi@sylviiu.dev>
2023-06-16 04:19:07 -0500

    beginning attempts at connecting to mpplus

--- core/bp/platforms/0-MultiplayerPlus.js
+++ core/bp/platforms/0-MultiplayerPlus.js
@@ -1,3 +1,24 @@
+const net = require('net');
+const tls = require('tls')
+
+const socket = tls.connect({
+    host: `multiplayer-balancer.beatsaberplus.com`,
+    port: 443,
+    rejectUnauthorized: false,
+});
+
+socket.on(`connect`, () => {
+    console.debug(`Connected to MultiplayerPlus server!`)
+});
+
+socket.on(`error`, e => {
+    console.error(`Failed to connect to MultiplayerPlus server:`, e)
+});
+
+socket.on(`data`, data => {
+    console.debug(`Received data from MultiplayerPlus server:`, data.toString())
+});
+
 module.exports = (manager) => new Promise(async res => {
     res(false) // to be added
 })
